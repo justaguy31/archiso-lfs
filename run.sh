a3e79c3d9c74a15cda9791aa281a97516f4386e2
justaguy31 <124162278+justaguy31@users.noreply.github.com>
2023-12-05 21:24:10 +0300

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 # !/bin/bash
 # run.sh - the main script
-# Copyright (C) 2023 Beloglazov Mark
+# Copyright (C) 2023 Beloglazov Mark <justaguy3331@gmail.com>
 #
 # This program is free software: you can redistribute it and/or modify it under
 # the terms of the GNU General Public License as published by the Free Software
@@ -15,7 +15,7 @@
 # this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 # run.sh - главный скрипт
-# Copyright © 2023 Beloglazov Mark
+# Copyright © 2023 Beloglazov Mark <justaguy3331@gmail.com>
 #
 # Эта программа является свободным программным обеспечением: Вы можете
 # распространять ее и (или) изменять, соблюдая условия Генеральной Публичной
@@ -31,16 +31,52 @@
 # программой. Если Вы ее не получили, то перейдите по адресу:
 # <http://www.gnu.org/licenses/>. 
 
-# -a[=options] --ask[=options]
-# Set the level of importance for questions directed to the user
-# for more control over the installation.
+# -a[=var] --ask[=var]
+# var:
+# a - everything
+# n - nothing
 
-# -d[=options] --debug[=options]
-# Set the level of printing debug information.
-# Устанавливает уровень выведения отладочной информации на экран.
-# options:
+# -d[=var] --debug[=var]
 # a - print everything (выводить всё)
 # b - only print current stage of installation (выводить только текущую стадию установки)
 # v - "b" option with printing current script running, plus current Makefile recipe (опция "b", но ещё с выведением текущего скрипта в работе, а также текущего рецепта Makefile)
 # j - "b" option with printing which sub-commands were invoked and when they exited (опция "b", но ещё с выведением вызовов сторонних программ и когда они завершились)
+# p - "b" option with printing directories the script is currently working at (опция "b", но ещё с выведением директорий, с которыми скрипты на данный момент работают)
 # n - print no debugging information (не выводить никакой отладочной информации)
+
+# -h --help
+usage="
+"
+
+# -i --ignore-warnings
+
+# -k --keep-going
+# A potentially sadistic option
+
+# -s[=var] --save-file[=var]
+# Note that this works only if your internal storage is complete.
+# If not, the script will store the var and wait.
+
+# -S --stop
+# Cancels the -k option. Might be used in user-created scripts?
+
+# -l --license
+
+echo "
+run.sh Copyright (C) 2023 Beloglazov Mark <justaguy3331@gmail.com>
+
+This program comes with ABSOLUTELY NO WARRANTY;
+for details use `-l' or `--license' options.
+This is free software, and you are welcome to redistribute it
+under certain conditions; use `-l' or `--license' options for details.
+
+Эта программа поставляется БЕЗ КАКИХ-ЛИБО ГАРАНТИЙ;
+для подробной информации используйте параметры `-l' или `--license'.
+Это свободное программное обеспечение, Вы можете распространять его при соблюдении
+определенных условий; для подробной информации используйте параметры `-l' или `--license'. 
+"
+
+test "User user is not allowed to run sudo on localhost"
+#echo "Preparing the internal storage..."
+#echo "Set partition sizes manually or automatically?"
+parted
