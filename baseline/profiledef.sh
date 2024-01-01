4dc195179bfe7f12731762b3e0d889ff7fe3dfe6
justaguy31 <124162278+justaguy31@users.noreply.github.com>
2024-01-01 13:25:36 +0300

    Update profiledef.sh

--- baseline/profiledef.sh
+++ baseline/profiledef.sh
@@ -24,6 +24,7 @@ airootfs_image_tool_options=('-zlz4hc,12' -E ztailpacking)
 # "1234" works
 encryption_key=
 # Persistent space in kilobytes (KB)
+# 30GB set as a test
 persistent_size=30000000
 keys_image_type="erofs"
 keys_image_tool_options=('-zlz4hc,12' -E ztailpacking)
