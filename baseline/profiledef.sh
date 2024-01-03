3de5aeaa9276f690bc029abd7f0eb2c40b1cc2b4
justaguy31 <124162278+justaguy31@users.noreply.github.com>
2024-01-03 18:41:03 +0300

    Update profiledef.sh
    Found these out by dancing with a timbrel
    
    Signed-off-by: justaguy31 <124162278+justaguy31@users.noreply.github.com>

--- baseline/profiledef.sh
+++ baseline/profiledef.sh
@@ -18,14 +18,13 @@ arch="x86_64"
 pacman_conf="pacman.conf"
 airootfs_image_type="erofs"
 airootfs_image_tool_options=('-zlz4hc,12' -E ztailpacking)
-# encryption key mode "auto" is bugged
-# leaving this variable empty prompts for user input
-# WARNING: missing information about acceptable keys
-# "1234" works
-encryption_key=
+# Setting encryption_key to `"auto"' will auto-generate the key.
+# Leaving the variable empty will prompt the user for the key.
+encryption_key=1234
 # Persistent space in kilobytes (KB)
 # 30GB set as a test
-persistent_size=30000000
+# Minimal persistent_size allowed: 16748
+persistent_size=16748
 keys_image_type="erofs"
 keys_image_tool_options=('-zlz4hc,12' -E ztailpacking)
 file_permissions=(
