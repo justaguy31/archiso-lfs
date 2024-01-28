7664ee4205f153e0d0eaa1e946c1a45397ee8992
justaguy31 <124162278+justaguy31@users.noreply.github.com>
2024-01-28 14:09:19 +0300

    Update systemd-network-init.sh
    Signed-off-by: justaguy31 <124162278+justaguy31@users.noreply.github.com>

--- releng/airootfs/etc/profile.d/systemd-network-init.sh
+++ releng/airootfs/etc/profile.d/systemd-network-init.sh
@@ -1,5 +1,7 @@
 echo "Setting up NetworkManager..."
 systemctl enable NetworkManager.service
 systemctl start NetworkManager.service
-echo "Initializing pacman... May fail if additional configuration required."
+echo "Initializing pacman..."
+echo "If it fails, run nmtui or nmcli for some manual configuration, such as entering password for your Wi-Fi router."
+echo "When you're done, run 'systemctl start pacman-init.service' in order to rerun pacman initialization."
 systemctl start pacman-init.service
