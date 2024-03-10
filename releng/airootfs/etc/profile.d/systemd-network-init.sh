6468ba801675bf016ee5aa18bcfde0d25d832899
justaguy31 <doktorigr222@gmail.com>
2024-03-10 13:46:27 +0300

    For detecting which pacman mirrors to use

--- releng/airootfs/etc/profile.d/systemd-network-init.sh
+++ releng/airootfs/etc/profile.d/systemd-network-init.sh
@@ -5,3 +5,8 @@ echo "Initializing pacman..."
 echo "If it fails, run nmtui or nmcli for some manual configuration, such as entering password for your Wi-Fi router."
 echo "When you're done, run 'systemctl start pacman-init.service' in order to rerun pacman initialization."
 systemctl start pacman-init.service
+# I have some big trouble modifying this because I have Windows 10,
+# so I'm just gonna leave this here.
+# archiso-lfs won't be living long anyways
+# as I want it to be replaced with an actual installation image of future LinuxHome distribution.
+chmod a+rwx /usr/local/bin/lfs-install
